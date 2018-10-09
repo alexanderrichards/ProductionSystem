707ba9470d9bc06503f436f6e90f3241f1c7f99f
Alexander Richards <a.richards@imperial.ac.uk>
2018-10-09 17:01:05 +0100

    Change entrypoint name to match setup.py

--- scripts/dirac-daemon.py
+++ scripts/dirac-daemon.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
                                    "(debugging only)")
     projects = set(entry_point.dist.project_name for entry_point in
                    chain(pkg_resources.iter_entry_points('dbmodels'),
-                         pkg_resources.iter_entry_points('daemons'),
+                         pkg_resources.iter_entry_points('webapp'),
                          pkg_resources.iter_entry_points('webapp.services')))
     projects -= {'productionsystem'}
     if projects:
