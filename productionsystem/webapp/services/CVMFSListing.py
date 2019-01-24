c4e4c5d395feb6340f1dd67244816139875e01ed
Alexander Richards <a.richards@imperial.ac.uk>
2019-01-24 10:51:05 +0000

    Add sorting (including natural version sorting) ability to CVMFSDirectoryListing service.

--- productionsystem/webapp/services/CVMFSListing.py
+++ productionsystem/webapp/services/CVMFSListing.py
@@ -1,6 +1,7 @@
 """CVMFS Directory Listing Service."""
 import os
 import re
+from distutils.version import StrictVersion
 import cherrypy
 from productionsystem.apache_utils import check_credentials
 
@@ -8,6 +9,9 @@ from productionsystem.apache_utils import check_credentials
 @cherrypy.expose
 class CVMFSDirectoryListing(object):
     """CVMFS Directory listing service."""
+
+    sort_type_map = {None: None,
+                     'versions': StrictVersion}
 
     def _cp_dispatch(self, vpath):
         cherrypy.request.params['path'] = os.path.join(*vpath)
@@ -32,6 +36,28 @@ class CVMFSDirectoryListing(object):
                                      "Bad type: expected one of ('dirs', 'files', 'all'), got %s"
                                      % list_type)
 
+        sort = data.get("sort", False)
+        if not isinstance(sort, bool):
+            raise cherrypy.HTTPError(400,
+                                     "Bad type: sort parameter to be of type bool, got (%r, %s)"
+                                     % (sort, type(sort)))
+        sort_reversed = data.get("sort-reversed", False)
+        if not isinstance(sort_reversed, bool):
+            raise cherrypy.HTTPError(400,
+                                     "Bad type: sort-reversed parameter to be of type bool, "
+                                     "got (%r, %s)" % (sort, type(sort)))
+        if sort and sort_reversed:
+            raise cherrypy.HTTPError(400,
+                                     "Bad request, can't set both sort and sort-reversed "
+                                     "parameters to True.")
+        sort_type = data.get("sort-type", None)
+        if sort_type not in CVMFSDirectoryListing.sort_type_map:
+            raise cherrypy.HTTPError(400,
+                                     "Bad type: expected sort-type to be one of %s, "
+                                     "got %s" % (CVMFSDirectoryListing.sort_type_map.keys(),
+                                                 sort_type))
+        sort_type = CVMFSDirectoryListing.sort_type_map[sort_type]
+
         target = os.path.join('/cvmfs', path)
         try:
             _, dirs, files = os.walk(target).next()
@@ -49,4 +75,7 @@ class CVMFSDirectoryListing(object):
                 match = regex.match(file_)
                 if match is not None:
                     output.append(match.group())
+
+        if sort or sort_reversed:
+            output.sort(reverse=sort_reversed, key=sort_type)
         return output
