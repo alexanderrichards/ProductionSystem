7541a9aaf9e16ed949694c1b69df2d9c8ce6d358
Alexander Richards <a.richards@imperial.ac.uk>
2019-01-31 16:26:24 +0000

    Remove dead file.

--- productionsystem/start-webapp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. $HOME/git/ProductionSystem/venv/bin/activate
-#$HOME/LZProduction/scripts/start-webserver.py -p18443 -d"mysql+pymysql://lzprod:JuvMoafcug2@localhost/lzprod"
-$HOME/git/ProductionSystem/scripts/webapp-daemon.py -p8080
