12fc37480a56c1bc048a69e516c2d52476a60d1a
Alexander Richards <a.richards@imperial.ac.uk>
2019-02-04 12:43:24 +0000

    More PEP8 fixes.

--- tests/test_stuff.py
+++ tests/test_stuff.py
@@ -4,11 +4,11 @@ import pkg_resources
 from productionsystem.config import ConfigSystem
 
 
-#def setup_module(module):
-#    """ setup any state specific to the execution of the given module."""
-#    config_instance = ConfigSystem.setup(None)  # pylint: disable=no-member
-#    config_instance.entry_point_map = pkg_resources.get_entry_map('productionsystem')
-#    return config_instance
+# def setup_module(module):
+#     """ setup any state specific to the execution of the given module."""
+#     config_instance = ConfigSystem.setup(None)  # pylint: disable=no-member
+#     config_instance.entry_point_map = pkg_resources.get_entry_map('productionsystem')
+#     return config_instance
 
 
 class TestStuff(TestCase):
