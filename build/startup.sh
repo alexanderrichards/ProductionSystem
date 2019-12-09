6d369812212d3ec39691b4a2d0893dc0f0b3cdfc
Alexander Richards <a.richards@imperial.ac.uk>
2019-12-09 16:24:59 +0000

    Updating for CentOS8.

--- build/startup.sh
+++ build/startup.sh
@@ -1,4 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+trap "stop_daemon $1 && exit 0" SIGTERM
+
+function stop_daemon {
+    case "$1" in
+        dirac)
+            echo "*** Stopping DIRAC daemon. ***"
+            (. /root/dirac_ui/bashrc && dirac-daemon.py stop)
+            ;;
+        monitoring)
+            echo "*** Stopping monitoring daemon. ***"
+            monitoring-daemon.py stop
+            ;;
+        webapp)
+            echo "*** Stopping web app daemon. ***"
+            webapp-daemon.py stop
+            ;;
+        all)
+            stop_daemon dirac
+            stop_daemon monitoring
+            stop_daemon webapp
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            echo "Expected: {dirac|monitoring|webapp|all}"
+            exit 1
+    esac
+}
 
 function start_daemon {
     case "$1" in
@@ -8,16 +36,16 @@ function start_daemon {
             ;;
         monitoring)
             echo "*** Starting monitoring daemon. ***"
-            (. /root/venv3/bin/activate && monitoring-daemon.py start)
+            monitoring-daemon.py start
             ;;
         webapp)
             echo "*** Starting web app daemon. ***"
-            (. /root/venv3/bin/activate && webapp-daemon.py start)
+            webapp-daemon.py start
             ;;
         all)
-            start_daemon dirac
-            start_daemon monitoring
-            start_daemon webapp
+	    start_daemon dirac
+	    start_daemon monitoring
+	    start_daemon webapp
             ;;
         *)
             echo "Unknown parameter: $1"
@@ -27,4 +55,8 @@ function start_daemon {
 }
 
 start_daemon $1
-/bin/bash
+# Allow time to receive and process SIGTERM handler
+while true
+do
+sleep 5
+done
