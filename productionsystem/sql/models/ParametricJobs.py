ea9b80f13d76be293c8e2a3387d4cb3abc56e314
Alexander Richards <a.richards@imperial.ac.uk>
2021-10-11 14:29:41 +0100

    Fix system crash with missing dirac status (#63)
    * Fix system crash with missing dirac status
    
    Sometime Dirac jobs have empty string as their status which should be invalid. This fix catches this problem and avoids crashing the whole system. The status is just set to running so that the monitoring can try again.
    
    * Fix linting error

--- productionsystem/sql/models/ParametricJobs.py
+++ productionsystem/sql/models/ParametricJobs.py
@@ -267,6 +267,10 @@ class ParametricJobs(SQLTableBase):
                     self.logger.warning("Unknown DiracStatus: %s. Setting to UNKNOWN",
                                         monitored_jobs[job.id]['Status'].upper())
                     job.status = DiracStatus.UNKNOWN
+            if not isinstance(job.status, DiracStatus):
+                self.logger.error("Dirac job %r status is invalid type %r",
+                                  str(job.id), type(job.status))
+                job.status = DiracStatus.RUNNING
             statuses.update((job.status.local_status,))
 
         status = max(statuses)
