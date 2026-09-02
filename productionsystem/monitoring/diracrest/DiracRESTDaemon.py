eeda8db388d550251c3dc0044a03cca4c2b3dd59
Alexander Richards <a.richards@imperial.ac.uk>
2026-09-02 16:56:21 +0100

    TESTING

--- productionsystem/monitoring/diracrest/DiracRESTDaemon.py
+++ productionsystem/monitoring/diracrest/DiracRESTDaemon.py
@@ -24,4 +24,5 @@ class DiracDaemon(Daemonize):
     def main(self):
         """Run the HTTP server."""
         host, port = self._address
+        print(f"Starting DIRAC REST daemon on {host}:{port}")
         uvicorn.run(create_app(), host=host, port=port, log_config=None)
